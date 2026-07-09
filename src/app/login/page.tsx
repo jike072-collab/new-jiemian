6d2f1c4ea77fc1805d8e1921ed0f34033cbf0768
jike072-collab <jike072@gmail.com>
2026-07-09 17:05:12 +0800

    Prevent auth html long cache

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,8 @@
 import { CustomerLogin } from "@/components/customer-login";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function LoginPage() {
   return <CustomerLogin />;
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,8 @@
 import { CustomerLogin } from "@/components/customer-login";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function RegisterPage() {
   return <CustomerLogin initialMode="register" />;
 }
